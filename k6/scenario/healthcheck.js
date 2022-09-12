6e33a1bf696a6c05330430d322a1ae2663ab6a06
das08 <41512077+das08@users.noreply.github.com>
2022-09-12 23:45:10 +0900

    add k6 env settings

--- k6/scenario/healthcheck.js
+++ k6/scenario/healthcheck.js
@@ -2,12 +2,10 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export default function () {
-    const res = http.get(`http://rakutan-app:8081/hello`);
+    const res = http.get(`${__ENV.RAKUTAN_BOT_API}/hello`);
     check(res, {
         'status was 200': (r) => r.status === 200,
         'body was OK': (r) => r.body === "Hello World!!",
     });
     sleep(1);
 }
-
-//docker compose run --rm -T k6 run - < scenario/healthcheck.js --vus 2000 --duration 5s
